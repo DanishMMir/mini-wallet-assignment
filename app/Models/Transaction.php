27bfc89df557d6f8570582820b293a6c03827073
DanishMMir <mirdanishmajeed@gmail.com>
2025-11-17 16:49:27 +0530

    + create/update models + add recommended security advisory lib

---
+++ app/Models/Transaction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+
+class Transaction extends Authenticatable
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'amount',
+        'commission_fee',
+    ];
+}
